b6b4e1454a5e504ab0e197a8e284c91ae72784d4
Eledoux <lawanledoux@gmail.com>
2024-10-18 18:09:14 +0200

    fix test

--- src/faster_whisper_server/security.py
+++ src/faster_whisper_server/security.py
@@ -3,23 +3,22 @@ from typing import Annotated
 from fastapi import Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
-from faster_whisper_server.config import config
+from faster_whisper_server.dependencies import get_config
+
 
 auth_scheme = HTTPBearer(scheme_name="API key")
-API_KEY = config.api_key
+api_key = get_config().api_key # HACK
 
-if not API_KEY:
-
+if not api_key: # HACK
     def check_api_key() -> None:
         # if No API key is set, so we don't check for it.
         pass
-
 else:
 
     def check_api_key(api_key: Annotated[HTTPAuthorizationCredentials, Depends(auth_scheme)]) -> str:
         if api_key.scheme != "Bearer":
             raise HTTPException(status_code=403, detail="Invalid authentication scheme")
-        if api_key.credentials != API_KEY:
+        if api_key.credentials != api_key:
             raise HTTPException(status_code=403, detail="Unauthorized")
 
         return api_key.credentials
